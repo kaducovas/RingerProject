74d76cc0a7700838204f1a89db5218546571070b
Werner Freund <wsfreund@gmail.com>
2016-08-30 17:28:54 -0300

    Moved Miscellenea to misc and added other dependencies and a setup file.

---
+++ matlab/setup_matlab.sh
@@ -0,0 +1,24 @@
+
+# Get sourced script absolute path
+if test -n "`$SHELL -c 'echo $ZSH_VERSION'`"; then
+  script_place="$(dirname $(readlink -f "$0"))"
+	mshell="zsh"
+elif test -n "`$SHELL -c 'echo $BASH_VERSION'`"; then
+  script_place=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+	mshell="bash"
+elif test "$(basename "$SHELL")" = "zsh"; then
+  script_place="$(dirname $(readlink -f "$0"))"
+	mshell="zsh"
+elif test "$(basename "$SHELL")" = "bash"; then
+  script_place=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+	mshell="bash"
+else
+  printf "ERROR: Unsupported shell." >&2 && return 1;
+fi
+
+source "$script_place/../root/RootCoreMacros/common_shell_fcns.sh" || { echo "Couldn't load base shell environment." && return exit 1; }
+
+add_to_env MATLABPATH "$script_place"
+add_to_env MATLABPATH "$script_place/misc/classes"
+add_to_env MATLABPATH "$script_place/misc/libs"
+add_to_env MATLABPATH "$script_place/misc/scripts"
